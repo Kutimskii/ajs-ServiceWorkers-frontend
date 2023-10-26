32d8a5f2cf1616f849df6782f284aa93b1bbfa1a
kkk <kutimskyi@gmail.com>
2023-10-26 21:14:07 +0300

    init

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,29 @@
 if (navigator.serviceWorker) {
-	window.addEventListener('load', async () => {
-		try {
-			if (navigator.serviceWorker) {
-				await navigator.serviceWorker.register('service-worker.js');
-				console.log('sw registered');
-			}
-		} catch (e) {
-			console.log(e);
-		}
-	});
+  window.addEventListener("load", async () => {
+    try {
+      if (navigator.serviceWorker) {
+        await navigator.serviceWorker.register("service-worker.js");
+        console.log("sw registered");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  });
 }
 let result;
-const createRequest = async ()=>{
-	try{
-	const respone = await fetch('http://localhost:3000?method=allNews');
-	console.log(respone)
-	if(respone.status >= 200) {
-	document.querySelector('.modal_container').classList.remove('active')
-	document.querySelector('body').style.cursor = 'initial';
-	result = await respone.json()
-	document.querySelector('ul').innerHTML='';
-	result.forEach(element => {
-		document.querySelector('ul').insertAdjacentHTML('afterbegin',`<li class="news_card">
+const createRequest = async () => {
+  try {
+    const respone = await fetch("http://localhost:3000?method=allNews");
+    console.log(respone);
+    if (respone.status >= 200) {
+      document.querySelector(".modal_container").classList.remove("active");
+      document.querySelector("body").style.cursor = "initial";
+      result = await respone.json();
+      document.querySelector("ul").innerHTML = "";
+      result.forEach((element) => {
+        document.querySelector("ul").insertAdjacentHTML(
+          "afterbegin",
+          `<li class="news_card">
 		<h2 class="news_card_header">News header</h2>
 		<div class="news_content">
 			<div class="news_content_img">
@@ -31,18 +33,18 @@ const createRequest = async ()=>{
 		</div>
 		</div>
 		</h2>
-	</li>`)
-	});
- }else{
-	alert ('sdfdsf')
- }
-}catch(error){
-	document.querySelector('body').style.cursor = 'initial'
-	document.querySelector('.modal_container').classList.add('active')
-}
-	
-}
+	</li>`
+        );
+      });
+    } else {
+      alert("sdfdsf");
+    }
+  } catch (error) {
+    document.querySelector("body").style.cursor = "initial";
+    document.querySelector(".modal_container").classList.add("active");
+  }
+};
 createRequest();
-if(!result){
-	document.querySelector('body').style.cursor = 'wait';
+if (!result) {
+  document.querySelector("body").style.cursor = "wait";
 }
